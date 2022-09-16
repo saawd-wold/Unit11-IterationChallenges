a0c462501cd186d65a65460490e85282979605ae
Daniel Saaw <saawd@woldinghamschool.co.uk>
2022-09-16 11:18:02 +0100

    fix for 2

--- src/ForLoops2.java
+++ src/ForLoops2.java
@@ -8,7 +8,7 @@ public class ForLoops2 {
         // just add the counter declaration, size check and increment operation.
         // Hint: how can you calculate the smallest even number that is at least as large as limit?
         for (int x = ; ;) {
-            System.out.println((String) x);
+            System.out.println("" + x);
         }
     }
 
